5ca63263c688b7f017bbeb96a5dfdb7da45e297c
slopezme08 <126433288+slopezme08@users.noreply.github.com>
2023-06-03 20:53:18 -0500

    Add files via upload

---
+++ ejercicio_81_a4/Ejercicio_81_a4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio_81_a4;
+
+/**
+ *
+ * @author User
+ */
+public class Ejercicio_81_a4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
